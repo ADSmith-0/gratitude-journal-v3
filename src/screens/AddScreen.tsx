c4bb79944c31628cc331e1331a588728a11662ca
adam smith <adam00199382818@gmail.com>
2023-11-13 15:21:55 +0000

    added boilerplate for add screen

--- src/screens/AddScreen.tsx
+++ src/screens/AddScreen.tsx
@@ -1,10 +1,12 @@
-import { Text } from "react-native";
+import { TextInput } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Calendar from "../components/Calendar";
 
 const AddScreen = () => {
   return (
     <SafeAreaView>
-      <Text>Add</Text>
+      <Calendar />
+      <TextInput editable multiline numberOfLines={4} style={{ borderWidth: 1 }} />
     </SafeAreaView>
   )
 }
