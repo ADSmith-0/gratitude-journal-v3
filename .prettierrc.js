abf7e57c5a9d3de0f2c150bba3aea332658a97fb
adam smith <adam00199382818@gmail.com>
2023-11-17 01:29:10 +0000

    disabled useTabs to fix prettier issue

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: true,
-  tabWidth: 2,
-  trailingComma: 'all',
+	arrowParens: "avoid",
+	bracketSameLine: true,
+	bracketSpacing: true,
+	tabWidth: 2,
+	trailingComma: "all",
+	useTabs: false,
 };
