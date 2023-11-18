626508ae5c78691951480d02102a54650518d8fe
adam smith <adam00199382818@gmail.com>
2023-11-18 01:01:15 +0000

    added storage on add entry screen

--- src/screens/AddScreen.tsx
+++ src/screens/AddScreen.tsx
@@ -3,7 +3,9 @@ import { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Button from "../components/UI/Button";
 import Input from "../components/UI/Input";
+import { useStorage } from "../hooks/useStorage";
 import { colours, fontSize, styles } from "../styles";
+import { Entry } from "../types";
 import DateFormatter from "../utils/DateFormatter";
 const { flex_column, ph_10, pl_3 } = styles;
 
@@ -11,6 +13,10 @@ const AddScreen = () => {
   const [date, setDate] = useState<string>(
     new DateFormatter().toReadableDate(),
   );
+
+  const [entry, setEntry] = useState<string>();
+
+  const [entries, setEntries] = useStorage("entries", {} as Entry);
 
   const calendarOnTouch = () => {
     // TODO calendar input onClick
@@ -35,8 +41,22 @@ const AddScreen = () => {
         placeholder="Today I'm grateful for..."
         placeholderTextColor={colours.grey[700]}
         style={pl_3}
+        defaultValue={entries[date]}
+        value={entry}
+        onChangeText={text => setEntry(text)}
       />
-      <Button title="Submit" onPress={() => console.log("working")} />
+      <Button
+        title="Submit"
+        onPress={() =>
+          setEntries(
+            prevEntries =>
+              ({
+                ...prevEntries,
+                [date]: entry,
+              } as Entry),
+          )
+        }
+      />
     </SafeAreaView>
   );
 };
