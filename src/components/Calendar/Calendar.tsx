15daf0035a9f396e812eb28b35e6a4e6c53ad388
adam smith <adam00199382818@gmail.com>
2023-11-26 23:59:24 +0000

    calendar now displays days for a given month

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,7 +1,26 @@
-import { Text } from "react-native";
+import { View } from "react-native";
+import Text from "../UI/Text";
+import useDates from "../../hooks/useDates";
+import { styles } from "../../styles";
 
 const Calendar = () => {
-  return <Text>Calendar</Text>;
+  const dates = useDates();
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return (
+    <View style={styles.flex_column_center}>
+      <View style={styles.flex_row_center}>
+        {days.map(day => (
+          <Text key={day}>{day}</Text>
+        ))}
+      </View>
+      <View style={styles.flex_row_center}>
+        {dates.map(date => (
+          <Text>{date || " "}</Text>
+        ))}
+      </View>
+    </View>
+  );
 };
 
 export default Calendar;
