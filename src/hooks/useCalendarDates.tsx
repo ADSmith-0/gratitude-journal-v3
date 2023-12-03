56c6d60f7676c63ddefdd3b96f823b176325996a
adam smith <adam00199382818@gmail.com>
2023-12-03 13:40:54 +0000

    moved globals out of useCalendarDates hook to prevent data re-initialising

--- src/hooks/useCalendarDates.tsx
+++ src/hooks/useCalendarDates.tsx
@@ -1,41 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { DateContext } from "../context/DateContext/DateContext";
 import DateProcessor from "../utils/DateProcessor";
-
-const days = ["M", "T", "W", "T", "F", "S", "S"];
-const dates = [
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "11",
-  "12",
-  "13",
-  "14",
-  "15",
-  "16",
-  "17",
-  "18",
-  "19",
-  "20",
-  "21",
-  "22",
-  "23",
-  "24",
-  "25",
-  "26",
-  "27",
-  "28",
-  "29",
-  "30",
-  "31",
-];
+import { dates, days } from "../utils/global";
 
 const useCalendarDates = () => {
   const { date } = useContext(DateContext);
