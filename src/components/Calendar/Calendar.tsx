82031be8f497d42db459818cab5476b8ffe94c62
adam smith <adam00199382818@gmail.com>
2023-12-05 13:58:37 +0000

    removed style prefixes from style classes

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -3,9 +3,34 @@ import useCalendarDates from "../../hooks/useCalendarDates";
 import { styles } from "../../styles";
 import { days } from "../../utils/global";
 import Text from "../UI/Text";
-const { flex_row_center, fs_m } = styles;
+const {
+  bg_primary_800,
+  bg_grey_900,
+  bg_offWhite,
+  flex_row_center,
+  flex_wrap,
+  fs_m,
+  fs_s,
+  gap_3,
+  justify_content_center,
+  mb_4,
+  mt_5,
+  pv_3,
+  text_bold,
+  text_align_center,
+  text_grey_300,
+  text_grey_400,
+} = styles;
 import Button from "../UI/Button";
 import { DateRelativeToToday } from "../../types";
+
+const { row } = StyleSheet.create({
+  row: {
+    flexGrow: 0,
+    flexShrink: 1,
+    flexBasis: "11%",
+  },
+});
 
 const Calendar = () => {
   const calendarDates = useCalendarDates();
@@ -14,57 +39,47 @@ const Calendar = () => {
 
   return (
     <View
-      style={[
-        flex_row_center,
-        styles.justify_content_center,
-        styles.flex_wrap,
-        styles.mt_5,
-        styles.gap_3,
-      ]}>
+      style={[flex_row_center, justify_content_center, flex_wrap, mt_5, gap_3]}>
       {days.map((day, i) => (
         <Text
           key={day + i}
           style={[
-            calendarStyles.row,
-            styles.text_align_center,
-            styles.text_bold,
-            styles.fs_s,
-            styles.text_grey_300,
-            styles.mb_4,
+            row,
+            text_align_center,
+            text_bold,
+            fs_s,
+            text_grey_300,
+            mb_4,
           ]}>
           {day}
         </Text>
       ))}
-      {calendarDates.map(({ date, relativeToToday, hasEntry }, i) => (
-        <Button
-          key={`${date}${i}`}
-          variant={"secondary"}
-          buttonStyle={[
-            calendarStyles.row,
-            styles.pv_3,
-            styles.bg_grey_900,
-            hasEntry && styles.bg_primary_800,
-            relativeToToday === DateRelativeToToday.OUT_OF_BOUNDS &&
-              styles.bg_offWhite,
-          ]}
-          textStyle={[
-            fs_m,
-            relativeToToday === DateRelativeToToday.OUT_OF_BOUNDS &&
-              styles.text_grey_400,
-          ]}
-          title={date}
-        />
-      ))}
+      {calendarDates.map(({ date, relativeToToday, hasEntry }, i) =>
+        date === " " ? (
+          <Text key={`${date}${i}`} style={row} />
+        ) : (
+          <Button
+            key={`${date}${i}`}
+            variant={"secondary"}
+            buttonStyle={[
+              row,
+              pv_3,
+              bg_grey_900,
+              hasEntry && bg_primary_800,
+              relativeToToday === DateRelativeToToday.OUT_OF_BOUNDS &&
+                bg_offWhite,
+            ]}
+            textStyle={[
+              fs_m,
+              relativeToToday === DateRelativeToToday.OUT_OF_BOUNDS &&
+                text_grey_400,
+            ]}
+            title={date}
+          />
+        ),
+      )}
     </View>
   );
 };
-
-const calendarStyles = StyleSheet.create({
-  row: {
-    flexGrow: 0,
-    flexShrink: 1,
-    flexBasis: "11%",
-  },
-});
 
 export default Calendar;
