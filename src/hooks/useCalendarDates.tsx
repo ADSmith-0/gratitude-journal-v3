8c7e600bb37e794bf1c7342f302d097631b90002
adam smith <adam00199382818@gmail.com>
2023-12-05 12:37:27 +0000

    calendar now gets entry from db and months are cached

--- src/hooks/useCalendarDates.tsx
+++ src/hooks/useCalendarDates.tsx
@@ -1,56 +1,75 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { DateContext } from "../context/DateContext/DateContext";
 import { calendarDate, DateRelativeToToday } from "../types";
 import DateProcessor from "../utils/DateProcessor";
+import EntriesStorageInstance from "../utils/EntriesStorage";
+
+const calculateMonth = (dateProcessor: DateProcessor): calendarDate[] => {
+  const calendar: calendarDate[] = [];
+
+  dateProcessor.date.setDate(1);
+  const firstDay = dateProcessor.date.getDay();
+
+  let startBuffer = 1;
+  while (startBuffer < firstDay) {
+    calendar.push({
+      date: " ",
+      relativeToToday: DateRelativeToToday.OUT_OF_BOUNDS,
+      hasEntry: false,
+    });
+    startBuffer++;
+  }
+
+  const today = new Date().valueOf();
+  const currentMonth = dateProcessor.date.getMonth();
+
+  while (dateProcessor.date.getMonth() === currentMonth) {
+    calendar.push({
+      date: dateProcessor.date.getDate().toString(),
+      relativeToToday:
+        dateProcessor.date.valueOf() < today
+          ? DateRelativeToToday.BEFORE_TODAY
+          : DateRelativeToToday.OUT_OF_BOUNDS,
+      hasEntry: !!EntriesStorageInstance.get(dateProcessor.toReadableDate()),
+    });
+    dateProcessor.nextDay();
+  }
+
+  while (calendar.length !== 35 && calendar.length !== 42) {
+    calendar.push({
+      date: " ",
+      relativeToToday: DateRelativeToToday.OUT_OF_BOUNDS,
+      hasEntry: false,
+    });
+  }
+
+  return calendar;
+};
 
 const useCalendarDates = () => {
   const { date } = useContext(DateContext);
-  const dateProcessor = new DateProcessor(date);
-
   const [calendarDates, setCalendarDates] = useState<calendarDate[]>([]);
+  const month = useRef<number>();
+  const cache = useRef(new Map());
 
-  // biome-ignore lint/correctness/useExhaustiveDependencies: the rule is garbage
   useEffect(() => {
-    const calendar: calendarDate[] = [];
+    const dateProcessor = new DateProcessor(date);
+    const currentMonth = dateProcessor.date.getMonth();
 
-    dateProcessor.date.setDate(1);
-    const firstDay = dateProcessor.date.getDay();
-
-    let startBuffer = 1;
-    while (startBuffer < firstDay) {
-      calendar.push({
-        date: " ",
-        relativeToToday: DateRelativeToToday.OUT_OF_BOUNDS,
-        hasEntry: false,
-      });
-      startBuffer++;
+    if (currentMonth !== month.current) {
+      if (cache.current.has(currentMonth)) {
+        console.log("cache hit");
+        setCalendarDates(cache.current.get(currentMonth));
+      } else {
+        month.current = currentMonth;
+        const calendarMonth: calendarDate[] = calculateMonth(dateProcessor);
+        setCalendarDates(calendarMonth);
+        if (cache.current.size < 10) {
+          cache.current.set(currentMonth, calendarMonth);
+        }
+      }
     }
-
-    const monthLength = dateProcessor.getMonthLength();
-    const today = new Date().valueOf();
-
-    while (dateProcessor.date.getDate() < monthLength) {
-      calendar.push({
-        date: dateProcessor.date.getDate().toString(),
-        relativeToToday:
-          dateProcessor.date.valueOf() < today
-            ? DateRelativeToToday.BEFORE_TODAY
-            : DateRelativeToToday.OUT_OF_BOUNDS,
-        hasEntry: false,
-      });
-      dateProcessor.nextDay();
-    }
-
-    while (calendar.length !== 35 && calendar.length !== 42) {
-      calendar.push({
-        date: " ",
-        relativeToToday: DateRelativeToToday.OUT_OF_BOUNDS,
-        hasEntry: false,
-      });
-    }
-
-    setCalendarDates(calendar);
-  }, [dateProcessor.date.getMonth()]);
+  }, [date]);
 
   return calendarDates;
 };
