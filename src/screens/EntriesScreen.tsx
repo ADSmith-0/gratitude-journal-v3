b873cf4e2bfda925360e5784c5f9fca40e192b8c
adam smith <adam00199382818@gmail.com>
2024-01-10 17:56:49 +0000

    changed SafeAreaView to ContextWrapper

--- src/screens/EntriesScreen.tsx
+++ src/screens/EntriesScreen.tsx
@@ -1,11 +1,34 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Search } from "lucide-react-native";
+import { ReactElement, useState } from "react";
+import { View } from "react-native";
+import Cards from "src/components/Entries/Cards";
+import ContentWrapper from "src/components/Screen/ContentWrapper";
+import Button from "src/components/UI/Button";
+import Input from "src/components/UI/Input";
+import { colours, fontSize, styles } from "src/styles";
 
 const EntriesScreen = () => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const searchIcon = (pressed: boolean): ReactElement => (
+    <Search
+      color={pressed ? colours.grey[300] : colours.grey[100]}
+      size={fontSize.l}
+    />
+  );
+
   return (
-    <SafeAreaView>
-      <Text>Entries</Text>
-    </SafeAreaView>
+    <ContentWrapper>
+      <View style={[styles.mt_8, styles.mb_4]}>
+        <Input placeholder="Search" style={[styles.mh_5, styles.pl_5]} />
+        <Button
+          buttonStyle={[styles.absolute]}
+          variant="tertiary"
+          icon={searchIcon}
+        />
+      </View>
+      <Cards />
+    </ContentWrapper>
   );
 };
 
