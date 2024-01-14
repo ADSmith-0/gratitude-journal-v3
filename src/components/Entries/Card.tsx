bd85bca43f9031b74f5f196550087e0c0d1e2fcc
adam smith <adam00199382818@gmail.com>
2024-01-14 14:05:37 +0000

    updated styling and fixed error where content was hidden by bottom tab bar

--- src/components/Entries/Card.tsx
+++ src/components/Entries/Card.tsx
@@ -1,7 +1,8 @@
 import { View } from "react-native";
-import { styles } from "src/styles";
-import { DateString } from "src/types";
 import Text from "src/components/UI/Text";
+import { colours, styles } from "src/styles";
+import { DateString } from "src/types";
+const { bg_white, border_grey_800, br_1, bw_1, mb_5, p_5 } = styles;
 
 type props = {
   date: DateString;
@@ -11,7 +12,7 @@ type props = {
 const Card = ({ date, entry }: props) => {
   // console.log(date, entry);
   return (
-    <View style={[styles.bg_white, styles.p_5, styles.br_1, styles.mb_5]}>
+    <View style={[bg_white, p_5, br_1, bw_1, border_grey_800, mb_5]}>
       <Text>{date}</Text>
       <Text>{entry}</Text>
     </View>
