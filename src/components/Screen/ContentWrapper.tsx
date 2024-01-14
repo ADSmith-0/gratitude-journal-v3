3213132fc6dd11a0a4fca9d6ef008a7fc063ab17
adam smith <adam00199382818@gmail.com>
2024-01-14 14:03:25 +0000

    changed margin to padding

--- src/components/Screen/ContentWrapper.tsx
+++ src/components/Screen/ContentWrapper.tsx
@@ -13,10 +13,9 @@ const ContentWrapper = ({ style, children }: Props) => {
     <View
       style={[
         {
-          marginTop: insets.top,
-          marginLeft: insets.left,
-          marginRight: insets.right,
-          marginBottom: insets.bottom + bottomTabBarHeight,
+          paddingTop: insets.top,
+          paddingLeft: insets.left,
+          paddingRight: insets.right,
           paddingBottom: insets.bottom,
         },
         style,
