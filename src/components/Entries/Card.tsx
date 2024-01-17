85f02901ddd0b6f4c39a1105a47622c29492ba7c
adam smith <adam00199382818@gmail.com>
2024-01-17 15:04:44 +0000

    removed unused code

--- src/components/Entries/Card.tsx
+++ src/components/Entries/Card.tsx
@@ -1,6 +1,6 @@
 import { View } from "react-native";
 import Text from "src/components/UI/Text";
-import { colours, styles } from "src/styles";
+import { styles } from "src/styles";
 import { DateString } from "src/types";
 const { bg_white, border_grey_800, br_1, bw_1, mb_5, p_5 } = styles;
 
@@ -9,14 +9,11 @@ type props = {
   entry: string;
 };
 
-const Card = ({ date, entry }: props) => {
-  // console.log(date, entry);
-  return (
-    <View style={[bg_white, p_5, br_1, bw_1, border_grey_800, mb_5]}>
-      <Text>{date}</Text>
-      <Text>{entry}</Text>
-    </View>
-  );
-};
+const Card = ({ date, entry }: props) => (
+  <View style={[bg_white, p_5, br_1, bw_1, border_grey_800, mb_5]}>
+    <Text>{date}</Text>
+    <Text>{entry}</Text>
+  </View>
+);
 
 export default Card;
