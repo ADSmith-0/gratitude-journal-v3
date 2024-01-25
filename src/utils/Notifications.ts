d892cf60758c45ffb9426b85428fc0735d4c140a
adam smith <adam00199382818@gmail.com>
2024-01-25 19:03:50 +0000

    made notifications time based and recur daily

---
+++ src/utils/Notifications.ts
@@ -0,0 +1,47 @@
+import notifee, {
+  RepeatFrequency,
+  TimestampTrigger,
+  TriggerType,
+} from "@notifee/react-native";
+import DateProcessor from "src/utils/DateProcessor";
+
+function notifications() {
+  async function setup() {
+    // Request permissions (required for iOS)
+    await notifee.requestPermission();
+
+    // Create a channel (required for Android)
+    const channelId = await notifee.createChannel({
+      id: "default",
+      name: "Default Channel",
+    });
+
+    const dateProcessor = new DateProcessor();
+    dateProcessor.date.setHours(19);
+    dateProcessor.date.setMinutes(1);
+
+    const trigger: TimestampTrigger = {
+      type: TriggerType.TIMESTAMP,
+      timestamp: dateProcessor.date.getTime(),
+      repeatFrequency: RepeatFrequency.DAILY,
+    };
+
+    await notifee.createTriggerNotification(
+      {
+        id: "1",
+        title: "What are you grateful for today?",
+        body: "Take time to leave an entry",
+        android: {
+          channelId,
+        },
+      },
+      trigger,
+    );
+  }
+
+  return {
+    setup,
+  };
+}
+
+export default notifications;
