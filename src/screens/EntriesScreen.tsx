37a570d49e96f53ebb21bc2822d0c26a03429df5
adam smith <adam00199382818@gmail.com>
2024-02-03 14:05:30 +0000

    updated styling

--- src/screens/EntriesScreen.tsx
+++ src/screens/EntriesScreen.tsx
@@ -1,17 +1,24 @@
 import { Search } from "lucide-react-native";
-import { ReactElement, useState } from "react";
+import { ReactElement } from "react";
 import { View } from "react-native";
 import Cards from "src/components/Entries/Cards";
 import ContentWrapper from "src/components/Screen/ContentWrapper";
 import Button from "src/components/UI/Button";
 import Input from "src/components/UI/Input";
 import { colours, fontSize, styles } from "src/styles";
-const { absolute, flex_1, flex_column_center, gap_5, pl_5, mb_4, mh_5, mt_8 } =
-  styles;
+const {
+  absolute,
+  align_items_stretch,
+  flex_1,
+  flex_column_center,
+  gap_5,
+  pl_5,
+  mb_4,
+  mh_5,
+  mt_8,
+} = styles;
 
 const EntriesScreen = () => {
-  const [searchTerm, setSearchTerm] = useState<string>("");
-
   const searchIcon = (pressed: boolean): ReactElement => (
     <Search
       color={pressed ? colours.grey[300] : colours.grey[100]}
@@ -24,7 +31,8 @@ const EntriesScreen = () => {
   // NOTE: Change to Insights panel? analytics, commonly used words/terms etc.
 
   return (
-    <ContentWrapper style={[flex_column_center, gap_5, flex_1]}>
+    <ContentWrapper
+      style={[flex_column_center, align_items_stretch, gap_5, flex_1]}>
       <View style={[mt_8, mb_4]}>
         <Input placeholder="Search" style={[mh_5, pl_5]} />
         <Button buttonStyle={absolute} variant="tertiary" icon={searchIcon} />
