a4bfc813d582f2a94e534f710e5607e97fe839d3
adam smith <adam00199382818@gmail.com>
2024-02-03 14:06:57 +0000

    added notification switch to settings screen

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,7 +1,28 @@
+import { useContext } from "react";
+import { View } from "react-native";
+import Switch from "src/components/UI/Switch";
 import Text from "src/components/UI/Text";
+import { NotificationsEnabledContext } from "src/context/NotificationsEnabledContext/NotificationsEnabled";
+import { styles } from "src/styles";
+
+const { flex_column_center, flex_row_center, justify_content_around, mt_8 } =
+  styles;
 
 const SettingsScreen = () => {
-  return <Text>Settings</Text>;
+  const { notificationsEnabled, setNotificationsEnabled } = useContext(
+    NotificationsEnabledContext,
+  );
+  return (
+    <View style={[flex_column_center, mt_8]}>
+      <View style={[flex_row_center, justify_content_around]}>
+        <Text>Notifications</Text>
+        <Switch
+          isEnabled={notificationsEnabled}
+          onChange={isEnabled => setNotificationsEnabled(isEnabled)}
+        />
+      </View>
+    </View>
+  );
 };
 
 export default SettingsScreen;
