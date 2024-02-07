fd1842b26ef5b7e95fa17a0e98927e1958e747a0
adam smith <adam00199382818@gmail.com>
2024-02-07 14:57:48 +0000

    added styling and updated the component to use the new context

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,26 +1,55 @@
+import DateTimePicker from "@react-native-community/datetimepicker";
 import { useContext } from "react";
 import { View } from "react-native";
 import Switch from "src/components/UI/Switch";
 import Text from "src/components/UI/Text";
-import { NotificationsEnabledContext } from "src/context/NotificationsEnabledContext/NotificationsEnabled";
+import { ReminderContext } from "src/context/ReminderContext/ReminderContext";
 import { styles } from "src/styles";
 
-const { flex_column_center, flex_row_center, justify_content_around, mt_8 } =
-  styles;
+const {
+  bg_offWhite,
+  border_1,
+  border_grey_800,
+  br_1,
+  flex_column_center,
+  flex_row_center,
+  justify_content_between,
+  ph_8,
+  pv_5,
+  mh_7,
+  mt_8,
+} = styles;
 
 const SettingsScreen = () => {
-  const { notificationsEnabled, setNotificationsEnabled } = useContext(
-    NotificationsEnabledContext,
-  );
+  const { reminder, dispatch } = useContext(ReminderContext);
   return (
-    <View style={[flex_column_center, mt_8]}>
-      <View style={[flex_row_center, justify_content_around]}>
+    <View
+      style={[
+        flex_column_center,
+        mt_8,
+        br_1,
+        bg_offWhite,
+        border_1,
+        border_grey_800,
+        pv_5,
+        ph_8,
+        mh_7,
+      ]}>
+      <View style={[flex_row_center, justify_content_between]}>
         <Text>Notifications</Text>
         <Switch
-          isEnabled={notificationsEnabled}
-          onChange={isEnabled => setNotificationsEnabled(isEnabled)}
+          isEnabled={reminder.isEnabled}
+          onChange={isEnabled =>
+            dispatch({ action: isEnabled ? "enable" : "disable" })
+          }
         />
       </View>
+      {/* FIX: change value to be reminder.time */}
+      {reminder.isEnabled && (
+        <View>
+          <DateTimePicker mode="time" value={new Date()} />
+        </View>
+      )}
     </View>
   );
 };
