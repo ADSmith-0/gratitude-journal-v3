b460b33fa5056d9f5852477d3aafc98459391e90
adam smith <adam00199382818@gmail.com>
2024-02-11 23:55:59 +0000

    changed useStorage to useLocalStorage

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,11 @@
+import { MMKVLoader, useMMKVStorage } from "react-native-mmkv-storage";
+export const MMKV = new MMKVLoader()
+  .withInstanceID("local-storage")
+  .initialize();
+
+const useLocalStorage = (key: string, defaultValue: unknown) => {
+  const [value, setValue] = useMMKVStorage(key, MMKV, defaultValue);
+  return [value, setValue];
+};
+
+export default useLocalStorage;
