bd955975229785a8da4e7aaf248a167bd9c1ee18
adam smith <adam00199382818@gmail.com>
2024-02-17 17:16:55 +0000

    added type safety to useLocalStorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,28 @@
+import { useEffect } from "react";
+import { useMMKVStorage } from "react-native-mmkv-storage";
+import DateProcessor from "src/utils/DateProcessor";
+import MMKV from "src/utils/MMKV";
+import { removeReminder, setReminder } from "src/utils/Reminder";
 
-const useLocalStorage = (key: string, defaultValue: unknown) => {
-  const [value, setValue] = useMMKVStorage(key, MMKV, defaultValue);
-  return [value, setValue];
+export type LocalStorage = {
+  "reminder-config": {
+    isEnabled: boolean;
+    time: Date;
+  };
+};
+
+const useLocalStorage = <K extends keyof LocalStorage>(
+  key: K,
+  defaultValue?: LocalStorage[K],
+) => {
+  const [value, setValue] = useMMKVStorage<LocalStorage[K]>(
+    key,
+    MMKV,
+    defaultValue,
+  );
+
+
+  return [value, setValue] as const;
 };
 
 export default useLocalStorage;
