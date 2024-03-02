b7ea2bf3413c93a01b5b18ccb3e1be797b107725
adam smith <adam00199382818@gmail.com>
2024-03-02 13:11:36 +0000

    changed input border colour to a lighter grey

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -9,7 +9,16 @@ import {
 import { styles } from "src/styles";
 import Text from "src/components/UI/Text";
 
-const { bg_white, border_1, br_1, fs_m, mb_3, pl_5, text_grey_100 } = styles;
+const {
+  bg_white,
+  border_1,
+  border_grey_800,
+  br_1,
+  fs_m,
+  mb_3,
+  pl_5,
+  text_grey_100,
+} = styles;
 
 type Props = {
   label?: string;
@@ -21,7 +30,16 @@ const Input = ({ label = "", style, containerStyle, ...props }: Props) => (
   <View style={containerStyle}>
     {label && <Text style={mb_3}>{label}</Text>}
     <TextInput
-      style={[bg_white, text_grey_100, fs_m, border_1, br_1, pl_5, style]}
+      style={[
+        bg_white,
+        text_grey_100,
+        fs_m,
+        border_1,
+        border_grey_800,
+        br_1,
+        pl_5,
+        style,
+      ]}
       {...props}
     />
   </View>
