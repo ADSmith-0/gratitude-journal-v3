3674d4939c3ff7954c11a2a22c1a20f1c5903c71
adam smith <adam00199382818@gmail.com>
2024-03-10 23:14:44 +0000

    added temp banner debugging

--- src/screens/AccountScreen/LoginScreen.tsx
+++ src/screens/AccountScreen/LoginScreen.tsx
@@ -7,6 +7,7 @@ import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
 import EntriesStorage from "src/utils/EntriesStorage";
 import { useState } from "react";
 import { useFocusEffect } from "@react-navigation/native";
+import Banner from "src/components/UI/Banner";
 
 type Props = {
   user: FirebaseAuthTypes.User;
@@ -14,6 +15,7 @@ type Props = {
 
 const LoginScreen = ({ user }: Props) => {
   const [entriesCount, setEntriesCount] = useState<number>(0);
+  const [displayBanner, setDisplayBanner] = useState<boolean>(false);
 
   useFocusEffect(() => {
     setEntriesCount(
@@ -32,14 +34,29 @@ const LoginScreen = ({ user }: Props) => {
         styles.mh_10,
         styles.gap_4,
       ]}>
+      <Text variant="secondary" size="s">
+        Logged in as
+      </Text>
       <Text size="l">{user.email}</Text>
       <Box style={[styles.bg_white, styles.gap_4]}>
         <Text>
           {entriesCount} {entriesCount > 1 ? "Entries" : "Entry"}
         </Text>
-        <Text>23 Entries</Text>
+        {user.metadata.creationTime && (
+          <Text>
+            Account created on{" "}
+            {new Date(user.metadata.creationTime).toLocaleDateString("en-GB")}
+          </Text>
+        )}
       </Box>
       <Button title="Sign out" onPress={() => auth().signOut()} />
+      <Button
+        title="Delete account"
+        variant="secondary"
+        flavour="destructive"
+        onPress={() => setDisplayBanner(prev => !prev)}
+      />
+      {displayBanner && <Banner message="Banner" />}
     </ContentWrapper>
   );
 };
